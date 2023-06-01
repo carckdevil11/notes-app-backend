5ff7f435dc8feecf573dacbe608bd6f3012f72fe
Muhammad Yesa <muhammadyesa@Muhammads-MacBook-Pro.local>
2023-06-01 14:12:15 +0700

    [[

---
+++ src/notes.js
@@ -0,0 +1,10 @@
+const notes = [{
+    title: '123',
+    tags: [],
+    body: '123',
+    id: 's51olff-XVuET8kc',
+    createdAt: '2023-05-28T09:28:40.027Z',
+    updatedAt: '2023-05-28T09:28:40.027Z'
+  }];
+
+module.exports = notes;
